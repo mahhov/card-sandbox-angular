4b715bba8eed96497eb59a74d13780831a950ea1
m <mahhov1@gmail.com>
2017-10-28 09:38:44 -0700

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,33 @@ import {HttpClientModule} from "@angular/common/http";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
+import {RouterModule, Routes} from "@angular/router";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {AppComponent} from "./app";
 import {Editor} from "./editorComponent/editor";
 import {ScriptEditorService} from "./layer/scriptEditorService";
 import {ScriptRepostiory} from "./layer/scriptRepository";
-import {MainPanel} from "./mainPanelComponent/mainPanel";
 import {TableCanvas} from "./tableCanvasComponent/tableCanvas";
+
+const appRoutes: Routes = [
+    {
+        path: 'editor',
+        component: Editor
+    },
+    {
+        path: 'table',
+        component: TableCanvas,
+    },
+    {
+        path: '',
+        redirectTo: 'editor',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
     declarations: [
         AppComponent,
-        MainPanel,
         TableCanvas,
         Editor
     ],
@@ -21,7 +36,8 @@ import {TableCanvas} from "./tableCanvasComponent/tableCanvas";
         BrowserModule,
         NgbModule.forRoot(),
         FormsModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes, {enableTracing: true})
     ],
     bootstrap: [
         AppComponent
