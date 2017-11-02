df6e79b54c08c92c10a220ceaee3ade934da27d4
m <mahhov1@gmail.com>
2017-11-02 14:29:28 -0700

    authentication service

---
+++ src/app/layer/authenticationService.ts
@@ -0,0 +1,22 @@
+import {Injectable} from "@angular/core";
+import {AuthenticationRepostiory} from "./authenticationRepository";
+
+@Injectable()
+export class AuthenticationService {
+    private token: string;
+
+    constructor(private authenticationRepository: AuthenticationRepostiory) {
+    }
+
+    public createUser(username: string, password: string): void {
+        this.authenticationRepository.createUser(username, password).then((token: string): void => {
+            this.token = token;
+        });
+    }
+
+    public login(username: string, password: string): void {
+        this.authenticationRepository.getToken(username, password).then((token: string): void => {
+            this.token = token;
+        });
+    }
+}
