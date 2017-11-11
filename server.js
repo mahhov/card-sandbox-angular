613f7f2a0d1b14b77d4a8a368881c8c8c40eabdf
m <mahhov1@gmail.com>
2017-11-11 09:08:38 -0800

    support url paths

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
+app.use((req, res) => res.sendFile(__dirname + '/dist/index.html'));
 app.listen(process.env.PORT || 4200);
